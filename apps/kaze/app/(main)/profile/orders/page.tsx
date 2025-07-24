70dfb590d7ffb373cb8f329df6004d05a6d3696d
tiesen243 <ttien56906@gmail.com>
2025-07-24 23:12:07 +0700

    fix: build error

--- apps/kaze/app/(main)/profile/orders/page.tsx
+++ apps/kaze/app/(main)/profile/orders/page.tsx
@@ -6,6 +6,8 @@ import {
 } from '@/app/(main)/profile/orders/page.client'
 import { getQueryClient, HydrateClient, trpc } from '@/trpc/rsc'
 
+export const dynamic = 'force-dynamic'
+
 export default function AddressPage() {
   void getQueryClient().prefetchQuery(trpc.order.getUserOrders.queryOptions())
 
