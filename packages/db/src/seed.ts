1311d7c7a148d779a80eac87c036c3e06a40faca
tiesen243 <ttien56906@gmail.com>
2025-07-30 03:26:50 +0700

    chore: minor changes

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -21,7 +21,7 @@ async function main() {
         insertedCategories.find(
           (category) => category.name === product.category,
         )?.id ?? '',
-      sellerId: 'cmd8u77bfgqhevlj9h6ccwy67',
+      sellerId: '', // Replace with actual seller ID
     })),
   )
 }
