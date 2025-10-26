9d86666efe9b78531bedd2f10b3c088aac5487ab
tiesen243 <ttien56906@gmail.com>
2025-10-26 09:34:13 +0700

    fix: httpLink

--- apps/dashboard/src/trpc/react.tsx
+++ apps/dashboard/src/trpc/react.tsx
@@ -1,12 +1,7 @@
 import type { QueryClient } from '@tanstack/react-query'
 import * as React from 'react'
 import { QueryClientProvider } from '@tanstack/react-query'
-import {
-  createTRPCClient,
-  httpBatchStreamLink,
-  httpLink,
-  splitLink,
-} from '@trpc/client'
+import { createTRPCClient, httpBatchStreamLink, httpLink } from '@trpc/client'
 import { createTRPCContext } from '@trpc/tanstack-react-query'
 import SuperJSON from 'superjson'
 
@@ -32,30 +27,30 @@ function TRPCReactProvider({
   // eslint-disable-next-line @eslint-react/naming-convention/use-state
   const [trpcClient] = React.useState(() =>
     createTRPCClient<AppRouter>({
-      links: [
-        splitLink({
-          condition: () => env.NEXT_PUBLIC_TRPC_USE_STREAMING === 'true',
-          false: httpLink({
-            transformer: SuperJSON,
-            url: getBaseUrl() + '/api/trpc',
-            headers() {
-              const headers = new Headers()
-              headers.set('x-trpc-source', 'react-router')
-              headers.set('trpc-accept', 'application/json')
-              return headers
-            },
-          }),
-          true: httpBatchStreamLink({
-            transformer: SuperJSON,
-            url: getBaseUrl() + '/api/trpc',
-            headers() {
-              const headers = new Headers()
-              headers.set('x-trpc-source', 'react-router')
-              return headers
-            },
-          }),
-        }),
-      ],
+      links:
+        env.NEXT_PUBLIC_TRPC_USE_STREAMING === 'true'
+          ? [
+              httpBatchStreamLink({
+                transformer: SuperJSON,
+                url: getBaseUrl() + '/api/trpc',
+                headers() {
+                  const headers = new Headers()
+                  headers.set('x-trpc-source', 'react-router')
+                  return headers
+                },
+              }),
+            ]
+          : [
+              httpLink({
+                transformer: SuperJSON,
+                url: getBaseUrl() + '/api/trpc',
+                headers() {
+                  const headers = new Headers()
+                  headers.set('x-trpc-source', 'react-router')
+                  return headers
+                },
+              }),
+            ],
     }),
   )
 
