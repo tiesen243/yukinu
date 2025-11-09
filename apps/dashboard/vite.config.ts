1ba9458e56f3133ca013764bad055c9f7232fc21
tiesen243 <ttien56906@gmail.com>
2025-11-09 23:01:19 +0700

    fix(dashboard): work plssssss

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => ({
-  base: '/dashboard/',
   plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
   build: {
     assetsDir: 'dashboard/assets',
