c4713a4ad2df2a48c5047095adbdf8c76404a6e1
tiesen243 <ttien56906@gmail.com>
2025-12-08 01:40:04 +0700

    chore: change product details page

--- apps/web/app/(main)/[slug]/_components/product-description.tsx
+++ apps/web/app/(main)/[slug]/_components/product-description.tsx
@@ -6,24 +6,22 @@ export const ProductDescription: React.FC = () => {
   } = usePage()
 
   return (
-    <section className='rounded-lg bg-card p-6 shadow-md dark:border'>
-      <h2 className='mb-6 text-2xl font-semibold text-balance'>Overview</h2>
-
-      <p className='mb-8 overflow-x-auto text-base text-pretty whitespace-pre-wrap'>
+    <article className='flex flex-col gap-6 rounded-lg bg-card p-6 shadow-md dark:border'>
+      <h2 className='text-2xl font-semibold text-balance'>Overview</h2>
+      <p className='overflow-x-auto text-base text-pretty whitespace-pre-wrap'>
         {description?.split('\\n').join('\n')}
       </p>
 
-      <h3 className='mb-6 text-2xl font-semibold text-balance'>
-        Specifications
-      </h3>
-
-      <ul className='space-y-2 text-pretty capitalize'>
-        {attributes.map((attr) => (
-          <li key={attr.name} className='text-lg'>
-            <strong>{attr.name}:</strong> {attr.value}
-          </li>
-        ))}
-      </ul>
-    </section>
+      <section className='flex flex-col gap-4'>
+        <h3 className='text-xl font-semibold text-balance'>Specifications</h3>
+        <ul className='flex flex-col gap-2 text-pretty capitalize'>
+          {attributes.map((attr) => (
+            <li key={attr.name}>
+              <strong>{attr.name}:</strong> {attr.value}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </article>
   )
 }
