b3db6b4b1d1a84318f8f00840a969d902adccff3
tiesen243 <ttien56906@gmail.com>
2025-12-29 13:08:27 +0700

    chore: minor changes

--- apps/mobile/src/uniwind-types.d.ts
+++ apps/mobile/src/uniwind-types.d.ts
@@ -2,9 +2,7 @@
 /// <reference types="uniwind/types" />
 
 declare module 'uniwind' {
-    export interface UniwindConfig {
-        themes: readonly ['light', 'dark']
-    }
+  export interface UniwindConfig {
+    themes: readonly ['light', 'dark']
+  }
 }
-
-export {}
