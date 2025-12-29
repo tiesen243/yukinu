da321f25e24c93c2b8d1bd4921ade6b2c7f37802
tiesen243 <ttien56906@gmail.com>
2025-12-30 02:04:34 +0700

    chore: minor changes

--- apps/mobile/src/app.tsx
+++ apps/mobile/src/app.tsx
@@ -6,13 +6,12 @@ import {
   Geist_600SemiBold,
   Geist_700Bold,
 } from '@expo-google-fonts/geist'
-import { Assets as NavigationAssets } from '@react-navigation/elements'
 import { DarkTheme, DefaultTheme } from '@react-navigation/native'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { Asset } from 'expo-asset'
 import { useFonts } from 'expo-font'
 import { createURL } from 'expo-linking'
 import * as SplashScreen from 'expo-splash-screen'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Provider as ReduxProvider } from 'react-redux'
 import { useUniwind } from 'uniwind'
 
@@ -21,9 +20,6 @@ import { Navigation } from '@/screens/__root'
 import { store } from '@/store'
 
 SplashScreen.preventAutoHideAsync()
-
-Asset.loadAsync([...NavigationAssets, require('../assets/icon.png')])
-import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 const prefix = createURL('/')
 
