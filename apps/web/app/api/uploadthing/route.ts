a9dad8ad4a54d8e93940f87fa413a4d2bc93bb0b
tiesen243 <ttien56906@gmail.com>
2026-02-04 21:04:55 +0700

    fix: typecheck

--- apps/web/app/api/uploadthing/route.ts
+++ apps/web/app/api/uploadthing/route.ts
@@ -1,7 +1,14 @@
+import type { NextRequest } from 'next/server'
+
 import { createNextRouteHandler } from '@yukinu/uploadthing'
 import { ourFileRouter, config } from '@yukinu/uploadthing/config'
 
-export const { GET, POST } = createNextRouteHandler({
-  router: ourFileRouter,
-  config,
-})
+type NextRouteHandler = (
+  request: NextRequest,
+) => void | Response | Promise<void | Response>
+
+export const { GET, POST }: { GET: NextRouteHandler; POST: NextRouteHandler } =
+  createNextRouteHandler({
+    router: ourFileRouter,
+    config,
+  })
