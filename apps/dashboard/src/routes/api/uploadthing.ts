f578af93befe7d45485d2a9e705ac615b34b41d5
tiesen243 <ttien56906@gmail.com>
2026-03-06 21:51:44 +0700

    chore: minor changes

--- apps/dashboard/src/routes/api/uploadthing.ts
+++ apps/dashboard/src/routes/api/uploadthing.ts
@@ -1,3 +1,5 @@
+// oxlint-disable prefer-destructuring
+
 import { createRemixRouteHandler } from '@yukinu/uploadthing'
 import { ourFileRouter, config } from '@yukinu/uploadthing/config'
 
@@ -5,5 +7,5 @@ export const handlers = createRemixRouteHandler({
   router: ourFileRouter,
   config,
 })
-export const { loader } = handlers
-export const { action } = handlers
+export const loader = handlers.loader
+export const action = handlers.action
