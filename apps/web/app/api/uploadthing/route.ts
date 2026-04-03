5d09c232cd9deaec9b66b59c29ed470f442a22a9
tiesen243 <ttien56906@gmail.com>
2026-04-04 00:16:34 +0700

    chore: bump deps

--- apps/web/app/api/uploadthing/route.ts
+++ apps/web/app/api/uploadthing/route.ts
@@ -1,14 +1,10 @@
-import type { NextRequest } from 'next/server'
+// oxlint-disable-next-line typescript/ban-ts-comment
+// @ts-nocheck
 
 import { createNextRouteHandler } from '@yukinu/uploadthing'
 import { ourFileRouter, config } from '@yukinu/uploadthing/config'
 
-type NextRouteHandler = (
-  request: NextRequest,
-  // oxlint-disable-next-line typescript/no-invalid-void-type
-) => void | Response | Promise<void | Response>
-
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
   config,
-}) as { GET: NextRouteHandler; POST: NextRouteHandler }
+})
